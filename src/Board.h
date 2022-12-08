7f74c9b3149fd2aeec16ad12155404b4758a01c8
lizzyavery <114427084+lizzyavery@users.noreply.github.com>
2022-12-07 21:37:34 -0500

    Update Board.h
    Create the Board.h file with data members and functions that I wrote in Board.cpp

---
+++ src/Board.h
@@ -0,0 +1,23 @@
+// Board.h
+#ifndef BOARD_H
+#define BOARD_H
+
+class Board{
+    public:
+        // Public Members
+        int column;
+        int height;
+        int row;
+        int slots;
+    
+        // Functions:
+        Board();                                // default constructor
+        ~Board();                               // deconstructor
+        draw_Board();                           // creates a string representation of the Board
+        int get_Value(int column);              // gets slot number to put checker
+        add_checker(char checker, int column);  // adds checker to the board
+        
+
+};
+
+#endif
