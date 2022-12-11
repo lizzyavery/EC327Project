f224c9886b09a0ba7c6ac660560e8ecfca393605
lizzyavery <114427084+lizzyavery@users.noreply.github.com>
2022-12-10 19:35:49 -0500

    Update Board.h
    edited data members

--- src/Board.h
+++ src/Board.h
@@ -6,17 +6,19 @@ class Board{
     public:
         // Public Members
         int column;
-        int height;
         int row;
-        int slots;
+        char checker;
     
         // Functions:
         Board();                                        // default constructor
-        ~Board();                                       // deconstructor
+        ~Board();                                       // destructor
         void draw_Board();                              // creates a string representation of the Board
         int get_Value(int column);                      // gets slot number to put checker
         void add_checker(char checker, int column);     // adds checker to the board
-        
+        bool is_down_diagonal_win(char checker);        // check if diagonal down win
+        bool is_up_diagonal_win (char checker);         // check if diagonal up win
+        bool is_vertical_win (char checker);            // check if vertical win
+        bool is_horizontal_win (char checker);          // check if horiztonal win
 
 };
 
