5560f75888d85be030bcbe75d120ec5a42c22cec
lizzyavery <114427084+lizzyavery@users.noreply.github.com>
2022-12-10 19:32:32 -0500

    Update Board.cpp
    fixed the draw_board() function and started to debug the add_checker() function. Still need to figure out a way to get the move to show on the board.

--- src/Board.cpp
+++ src/Board.cpp
@@ -6,20 +6,15 @@
 #include <iostream>
 using namespace std;
 
+// number of slots
+char slots[6][7] = {' '};
+
 // Default Constructor
 Board :: Board() {
     row = 6;
-    width = 6;
     column = 7;
-    height = 7;
 }
 
-Board :: Board(int in_width, int in_height){
-    row = width;
-    column = height;
-    width = in_width;
-    height = in_height;
-}
 
 // Destructor
 Board :: ~Board(){
@@ -29,43 +24,41 @@ Board :: ~Board(){
 // FUNCTION TO DISPLAY THE CONNECT 4 BOARD 
 void Board :: draw_Board() {
 
-    // number of slots
-    char slots [row][column] = {' '};
-
     cout << endl;
     // Display header of the board with column numbers
     for (int i = 1; i <= column; i++){
-        cout << "   " << i <<  "   ";
+        cout << "   " << i <<  "  ";
     }
     cout << endl;
-    cout << string(50,'-') << endl;
+    cout << string(45,'-') << endl;
 
     // add one row of slots at a time
     for (int i = 0; i < row; i++){
         cout << "|  ";                                   // one vertical bar at the start
-        for (int j = 0; j < column; j++){
+        for (int j = 0; j < column-1; j++){
             cout << slots[row][column]  << "   |  ";
         }
-        cout << endl;
-        cout << string(50,'-') << endl;
+        cout << "    |" << endl;
+        cout << string(45,'-') << endl;
     }
 }
+
 
 // FUNCTION TO ADD A CHECKER TO THE BOARD
 void Board :: add_checker(char checker, int column) {
     
     assert(checker == 'X' || checker == 'O');
-    assert(0 <= column < board.column);
+    assert(0 <= column < 7);
 
     
-    row = 0
-    while ((slots[row][col] == ' ') && (row < height-1) && (slots[row+1][col] == ' ')){
-       row += 1 
+    int row_num = 0;
+    while ((slots[row_num][column] == ' ') && (row_num < (5)) && (slots[row_num+1][column] == ' ')){
+       row += 1;
     }
 
-    slots[row][col] = checker;
-        
-
+    slots[row][column] = checker; 
+    // redraw board with added checker:
+    draw_Board();
 }
 
 // FUNCTION TO CHECK IF THERE IS A DIAGNOGNAL WIN (DOWN)
@@ -92,6 +85,7 @@ bool Board :: is_vertical_win(char checker){
 // Test and Debug
 int main(){
     Board board;
-    board.draw_Board();
+    //board.draw_Board();
+    board.add_checker('X', 3);
     return 0;
 }
