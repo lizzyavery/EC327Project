c255b752043eba7760b193f8b2cbd1018a6db680
gimmymansui <114517092+gimmymansui@users.noreply.github.com>
2022-12-11 20:51:25 -0500

    Board is fine now

--- src/Board.cpp
+++ src/Board.cpp
@@ -4,88 +4,219 @@
 #include <assert.h>
 #include <cstring>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 // number of slots
-char slots[6][7] = {' '};
-
+char slots[6][7] =
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',
+ ' ',' ',' ',' ',' ',' ',' ',
+ ' ',' ',' ',' ',' ',' ',' ',' ',
+ ' ',' ',' ',' ',' ',' ',' ',' ',' ',
+ ' ',' ',' ',' ',' ',' ',' ',' ',' '};
 // Default Constructor
 Board :: Board() {
-    row = 6;
-    column = 7;
+	row = 6;
+	column = 7;
+	checker =  'X';
+
+	cout << "Board of size " << column << ' ' << row << " was generated" << endl;
 }
 
 
 // Destructor
 Board :: ~Board(){
-    cout << "The destructor was called" << endl;
+	cout << "The destructor was called" << endl;
 }
 
-// FUNCTION TO DISPLAY THE CONNECT 4 BOARD 
+// FUNCTION TO DISPLAY THE CONNECT 4 BOARD
 void Board :: draw_Board() {
 
-    cout << endl;
-    // Display header of the board with column numbers
-    for (int i = 1; i <= column; i++){
-        cout << "   " << i <<  "  ";
-    }
-    cout << endl;
-    cout << string(45,'-') << endl;
+	cout << endl;
+	// Display header of the board with column numbers
+	for (int i = 1; i <= column; i++) {
+		cout << "   " << i <<  "  ";
+	}
+	cout << endl;
+	cout << string(45,'-') << endl;
 
-    // add one row of slots at a time
-    for (int i = 0; i < row; i++){
-        cout << "|  ";                                   // one vertical bar at the start
-        for (int j = 0; j < column-1; j++){
-            cout << slots[row][column]  << "   |  ";
-        }
-        cout << "    |" << endl;
-        cout << string(45,'-') << endl;
-    }
+	// add one row of slots at a time
+	for (int i = 0; i < row; i++) {
+		cout << "|  ";                                               // one vertical bar at the start
+		for (int j = 0; j < column; j++) {
+			cout << slots[i][j]  << "  |  ";
+		}
+		cout << endl;
+		cout << string(45,'-') << endl;
+	}
 }
 
 
 // FUNCTION TO ADD A CHECKER TO THE BOARD
 void Board :: add_checker(char checker, int column) {
-    
-    assert(checker == 'X' || checker == 'O');
-    assert(0 <= column < 7);
 
-    
-    int row_num = 0;
-    while ((slots[row_num][column] == ' ') && (row_num < (5)) && (slots[row_num+1][column] == ' ')){
-       row += 1;
-    }
+	assert(checker == 'X' || checker == 'O');
+	assert(0 <= column < 7);
 
-    slots[row][column] = checker; 
-    // redraw board with added checker:
-    draw_Board();
+	//int row_num = 0;
+	for (int i = row - 1; i >= 0; i--)
+	{
+		if(slots[i][column-1] == ' ')
+		{
+			slots[i][column-1] = checker;
+			break;
+		}
+	}
+
+	//slots[row][column] = checker;
+	// redraw board with added checker:
+	draw_Board();
+	check_win(checker);
+	win(check_win(checker));
 }
 
+void Board::reset()
+{
+	for (int i = 0; i < this->row; i++)
+	{
+		for (int j = 0; i < this->column; i++)
+		{
+			slots[i][j] = ' ';
+		}
+	}
+}
+
+void Board::add_checkers(int columns)
+{
+	bool boolval = can_add_to(columns);
+	if (boolval) {
+		if (this->checker == 'X')
+		{
+			Board::add_checker(checker, columns);
+			this->checker = 'O';
+		} else if(this->checker = 'O')
+		{
+			Board::add_checker(checker, columns);
+			this->checker = 'X';
+		}
+	} else if (!boolval) {
+		cout << "can't add" << endl;
+	}
+}
+
+
+bool Board::can_add_to(int columns)
+{
+	bool returnval = false;
+	for(int i = (this->row) - 1; i > 0; i--)
+	{
+		if (slots[i][columns - 1] == ' ')
+		{
+			returnval = true;
+			break;
+		}
+	}
+	//cout << returnval << "bool" << endl;
+	return returnval;
+}
+
+
+bool Board::check_win(char checker)
+{
+	char checkercheck = checker;
+	bool boolval = false;
+	for( int i = 6; i >= 1; --i )
+	{
+
+		for( int ix = 7; ix >= 1; --ix )
+		{
+
+			if( slots[i][ix] == checkercheck   &&
+			    slots[i-1][ix-1] == checkercheck &&
+			    slots[i-2][ix-2] == checkercheck &&
+			    slots[i-3][ix-3] == checkercheck )
+			{
+				boolval = true;
+			}
+
+
+			if( slots[i][ix] == checkercheck   &&
+			    slots[i][ix-1] == checkercheck &&
+			    slots[i][ix-2] == checkercheck &&
+			    slots[i][ix-3] == checkercheck )
+			{
+				boolval = true;
+			}
+
+			if( slots[i][ix] == checkercheck   &&
+			    slots[i-1][ix] == checkercheck &&
+			    slots[i-2][ix] == checkercheck &&
+			    slots[i-3][ix] == checkercheck )
+			{
+				boolval = true;
+			}
+
+			if( slots[i][ix] == checkercheck     &&
+			    slots[i-1][ix+1] == checkercheck &&
+			    slots[i-2][ix+2] == checkercheck &&
+			    slots[i-3][ix+3] == checkercheck )
+			{
+				boolval = true;
+			}
+
+			if ( slots[i][ix] == checkercheck   &&
+			     slots[i][ix+1] == checkercheck &&
+			     slots[i][ix+2] == checkercheck &&
+			     slots[i][ix+3] == checkercheck )
+			{
+				boolval = true;
+			}
+		}
+
+	}
+	return boolval;
+}
+
+void Board::win(bool wincon)
+{
+	if(wincon)
+	{
+		cout << "Connect Four! Game Over" << endl;
+		exit(0);
+	}
+}
 // FUNCTION TO CHECK IF THERE IS A DIAGNOGNAL WIN (DOWN)
-bool Board :: is_down_diagonal_win(char checker){
-    return false;
-}
+/*bool Board :: is_down_diagonal_win(char checker){
+          if
+          return false;
+   }
 
-// FUNCTION TO CHECK IF THERE IS A DIAGNOGNAL WIN (UP)
-bool Board :: is_up_diagonal_win(char checker){
-    return false;
-}
+   // FUNCTION TO CHECK IF THERE IS A DIAGNOGNAL WIN (UP)
+   bool Board :: is_up_diagonal_win(char checker){
+          return false;
+   }
 
-// FUNCTION TO CHECK IF THERE IS A HORIZONTAL WIN
-bool Board :: is_horizontal_win(char checker){
-    return false;
-}
+   // FUNCTION TO CHECK IF THERE IS A HORIZONTAL WIN
+   bool Board :: is_horizontal_win(char checker){
+          return false;
+   }
 
-// FUNCTION TO CHECK IF THERE IS A VERTICAL WIN
-bool Board :: is_vertical_win(char checker){
-    return false;
-}
+   // FUNCTION TO CHECK IF THERE IS A VERTICAL WIN
+   bool Board :: is_vertical_win(char checker){
+          return false;
+   }*/
 
 
 // Test and Debug
 int main(){
-    Board board;
-    //board.draw_Board();
-    board.add_checker('X', 3);
-    return 0;
+	int a;
+	Board board;
+	board.draw_Board();
+	while(true)
+	{
+		cout << "select a columns: ";
+		cin >> a;
+		board.add_checkers(a);
+	}
+	return 0;
 }
--- src/Board.h
+++ src/Board.h
@@ -8,17 +8,22 @@ class Board{
         int column;
         int row;
         char checker;
-    
+
         // Functions:
         Board();                                        // default constructor
         ~Board();                                       // destructor
         void draw_Board();                              // creates a string representation of the Board
-        int get_Value(int column);                      // gets slot number to put checker
+        int get_Value(int column);   // gets slot number to put checker
+        void reset(); //resets board
+        bool can_add_to(int column); // returns true if column can be added
+        void add_checkers(int columns);                  // alternates between the 'X' and 'O'
         void add_checker(char checker, int column);     // adds checker to the board
-        bool is_down_diagonal_win(char checker);        // check if diagonal down win
-        bool is_up_diagonal_win (char checker);         // check if diagonal up win
-        bool is_vertical_win (char checker);            // check if vertical win
-        bool is_horizontal_win (char checker);          // check if horiztonal win
+        bool check_win(char checker);
+        void win(bool wincon);
+        //bool is_down_diagonal_win(char checker);        // check if diagonal down win
+        //bool is_up_diagonal_win (char checker);         // check if diagonal up win
+        //bool is_vertical_win (char checker);            // check if vertical win
+        //bool is_horizontal_win (char checker);          // check if horiztonal win
 
 };
 
